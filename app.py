52506c8d9239ff0e71beb13c0322a39fec75aef2
Venkateswarao Tella <venkateswaraotella25@gmail.com>
2025-10-25 16:24:49 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/add', methods=['GET'])
+def add_numbers():
+    try:
+        a = int(request.args.get('a'))
+        b = int(request.args.get('b'))
+        result = a + b
+        return jsonify({"a": a, "b": b, "result": result})
+    except:
+        return jsonify({"error": "Please pass integers a and b"}), 400
+
+@app.route('/')
+def index():
+    return "Welcome to GCP DevOps Demo - Add Two Numbers API"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
